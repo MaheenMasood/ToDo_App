d6855591add9da6ab44aec93354f561ad3f3c7c1
MaheenMasood <seemamasood032@gmail.com>
2025-05-08 14:20:57 -0700

    Refactor 2: Extract API Endpoints to Constants
    Before:
    
    API URLs were hardcoded directly inside HandleApi.js.
    
    After:
    
    Created a constants.js file in utils/ to centralize all API endpoints.
    
    Replaced hardcoded paths in HandleApi.js with constants from API_ENDPOINTS.
    
    Applies to: HandleApi.js, utils/constants.js

--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -1,54 +1,53 @@
 import axios from 'axios';
+import { API_ENDPOINTS } from './constants';
+
 axios.defaults.baseURL = 'http://localhost:5000';
 
 const getAllToDo = (setToDo) => {
-    axios.get('/api/todos')
-        .then(({ data }) => {
-            console.log('data--->', data);
-            setToDo(data);
-        })
-        .catch((error) => {
-            console.error('Error fetching data: ', error);
-        });
+  axios.get(API_ENDPOINTS.GET)
+    .then(({ data }) => {
+      console.log('data--->', data);
+      setToDo(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data: ', error);
+    });
 };
 
 const addToDo = (text, setText, setToDo) => {
-    axios.post('/api/todos/save', { text })
-        .then(({ data }) => {
-            console.log(data);
-            setText('');
-            getAllToDo(setToDo);
-        })
-        .catch((error) => {
-            console.error('Error adding todo: ', error);
-        });
+  axios.post(API_ENDPOINTS.SAVE, { text })
+    .then(({ data }) => {
+      console.log(data);
+      setText('');
+      getAllToDo(setToDo);
+    })
+    .catch((error) => {
+      console.error('Error adding todo: ', error);
+    });
 };
 
-
 const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
-    axios.post('/api/todos/update', { _id: toDoId, text })
-        .then(({ data }) => {
-            console.log(data);
-            setText('');
-            setIsUpdating(false)
-            getAllToDo(setToDo);
-        })
-        .catch((error) => {
-            console.error('Error updating todo: ', error);
-        });
-
+  axios.post(API_ENDPOINTS.UPDATE, { _id: toDoId, text })
+    .then(({ data }) => {
+      console.log(data);
+      setText('');
+      setIsUpdating(false);
+      getAllToDo(setToDo);
+    })
+    .catch((error) => {
+      console.error('Error updating todo: ', error);
+    });
 };
 
 const deleteToDo = (_id, setToDo) => {
-    axios.post('/api/todos/delete', { _id })
-        .then(({ data }) => {
-            console.log(data);
-            getAllToDo(setToDo);
-        })
-        .catch((error) => {
-            console.error('Error deleting todo: ', error);
-        });
-
+  axios.post(API_ENDPOINTS.DELETE, { _id })
+    .then(({ data }) => {
+      console.log(data);
+      getAllToDo(setToDo);
+    })
+    .catch((error) => {
+      console.error('Error deleting todo: ', error);
+    });
 };
 
 export { getAllToDo, addToDo, updateToDo, deleteToDo };
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_BASE = '/api/todos';
+export const API_ENDPOINTS = {
+  GET: API_BASE,
+  SAVE: `${API_BASE}/save`,
+  UPDATE: `${API_BASE}/update`,
+  DELETE: `${API_BASE}/delete`,
+};
